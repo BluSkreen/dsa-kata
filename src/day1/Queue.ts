5c6f998b41bced28735d86b87ccddc570054456e
BluSkreen <jasonduran303@gmail.com>
2023-04-28 14:22:40 -0600

    lots..., graph list

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,52 @@
+type Node<T> = {
+    value: T,
+    next?: Node<T>,
+}
 export default class Queue<T> {
     public length: number;
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
     
 
     constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
     }
 
+    // push to back
     enqueue(item: T): void {
+        const node = {value: item} as Node<T>;
 
-}
+        this.length++;
+
+        // if 0 nodes in queue 
+        if(!this.tail) {
+            this.tail = this.head = node;
+        }
+
+        this.tail.next = node;
+        this.tail = node;
+    }
+
+    // pop from front
     deque(): T | undefined {
+        if(!this.head) {
+            return undefined;
+        }
 
-}
+        this.length--;
+
+        const head = this.head;
+        this.head = this.head.next;
+
+        // free
+        head.next = undefined;
+
+        return head.value;
+    }
+
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,71 @@
-export default function quick_sort(arr: number[]): void {
 
+function qs(arr: number[], lo: number, hi: number): void {
+    if (lo >= hi) {
+        return;
+    }
+    
+    const pivotIdx = partition(arr, lo, hi);
+
+    qs(arr, lo, pivotIdx - 1);
+    qs(arr, pivotIdx + 1, hi);
+}
+
+function partition(arr: number[], lo: number, hi: number): number {
+    const pivot = arr[hi];
+
+    let idx = lo - 1;
+
+    for (let i = lo; i < hi; ++i) {
+        if (arr[i] <= pivot) {
+            idx++;
+            const tmp = arr[i];
+            arr[i] = arr[idx];
+            arr[idx] = tmp;
+        }
+    }
+
+    idx++;
+    arr[hi] = arr[idx];
+    arr[idx] = pivot;
+
+    return idx;
+}
+
+/* function partition(arr: number[], lo: number, hi: number): number {
+    const mid = (Math.ceil((lo + hi) / 2));
+    console.log(`${lo}  ${hi}`);
+    console.log(arr);
+    console.log(mid);
+    const pivot = arr[mid];
+    console.log(pivot);
+
+    let past = false;
+    let idx = lo - 1;
+
+    for (let i = lo; i < hi + 1; i++) {
+        if (arr[i] <= pivot && !(i === mid)) {
+            idx++;
+            if (idx === mid){
+                idx++;
+                past = true;
+            }
+            const tmp = arr[i];
+            arr[i] = arr[idx];
+            arr[idx] = tmp;
+        }
+    }
+
+    if (!past) {
+        idx++;
+    }
+    arr[mid] = arr[idx];
+    arr[idx] = pivot;
+    console.log(arr);
+    console.log(idx);
+
+    return idx;
+}*/
+
+export default function quick_sort(arr: number[]): void {
+    qs(arr, 0, arr.length - 1);
 }
