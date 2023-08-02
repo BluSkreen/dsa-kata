09f6355b94c2f80ac6231c34dd4dac1f50630e42
BluSkreen <jasonduran303@gmail.com>
2023-08-02 12:12:07 -0600

    working on trie

--- src/day6/Trie.ts
+++ src/day6/Trie.ts
@@ -1,18 +1,43 @@
+type Node = {
+    value: string, 
+    isWord?: boolean, 
+    head?: Node, 
+    children?: Node[],
+}
+
 export default class Trie {
     
-
+    private wordCount: number;
+    private root?: Node;
     
 
     constructor() {
+        this.wordCount = 0;
+        this.root = undefined;
     }
 
     insert(item: string): void {
-
-}
+        if (!this.root) {
+            const root  = { value: "", children: [] } as Node;
+            this.root = root;
+        }
+        let curr = this.root;
+        for (let i = 0; i < item.length; i++) {
+            let cIdx = this.idx(item[i]);
+            if (curr.children[cIdx]) {
+                curr = curr.children[cIdx];
+            }
+        }
+    }
     delete(item: string): void {
 
-}
+    }
     find(partial: string): string[] {
 
-}
+    }
+
+    private idx(str: string): number { 
+        const zero = "a".charCodeAt(0);
+        return str.charCodeAt(0) - zero;
+    }
 }
