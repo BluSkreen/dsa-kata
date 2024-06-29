f137a7eead6b81b30ece55d5c67bd83fe52b49f2
BluSkreen <jasonduran303@gmail.com>
2024-06-29 11:50:40 -0600

    old/temp

--- src/day6/Trie.ts
+++ src/day6/Trie.ts
@@ -24,17 +24,45 @@ export default class Trie {
         let curr = this.root;
         for (let i = 0; i < item.length; i++) {
             let cIdx = this.idx(item[i]);
-            if (curr.children[cIdx]) {
+            if (curr.children && curr.children[cIdx]) {
                 curr = curr.children[cIdx];
+                continue;
             }
+            curr.children = [];
+            curr.children[cIdx] = { value: item[i] } as Node;
+            curr = curr.children[cIdx];
         }
+        curr.isWord = true;
     }
+
     delete(item: string): void {
-
     }
+
     find(partial: string): string[] {
+        let out: string[] = [];
+        if (!this.root) {
+            return [];
+        }
 
+        let curr = this.root;
+        for (let i = 0; i < partial.length; i++) {
+            let cIdx = this.idx(partial[i]);
+            if (curr.isWord) {
+                out.push(partial.slice(0, i + 1));
+            }
+            if (curr.children && curr.children[cIdx]) {
+                curr = curr.children[cIdx];
+                continue;
+            }
+            console.log(out);
+            return out;
+        }
+
+
+        console.log(out);
+        return out;
     }
+
 
     private idx(str: string): number { 
         const zero = "a".charCodeAt(0);
